cec5eedecf1afcd6ef53df41ad0e57023d9a6849
Alan Nava <alan.octavio.1260@gmail.com>
2025-10-21 00:06:10 -0600

    .

--- Car Controller - Unity/Assets/Scripts/PlayerController.cs
+++ Car Controller - Unity/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] protected float speed = 20;
+    public TextMeshProUGUI RestartButton;
+    public TextMeshProUGUI ScoreText;
+    public SceneManager sceneManager;
+    protected float speed = 20;
      private float turnspeed = 25;
      private float horizontalInput;
-     private float forwardInput;
-
+    private float forwardInput;
+    private int score = 0;
     // Start is called before the first frame update
     void Start()    
     {
@@ -25,5 +31,17 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // we turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
+        if (forwardInput != 0 ) RestartButton.gameObject.SetActive(false);
     }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void updateScore(int score)
+    {
+        this.score += score;
+        ScoreText.text = "Score: " + this.score;
+    }
+        
 }
